eae3636e669a26fe15eb79812821fe2719a882bc
Aztrob01 <galxy.sup@gmail.com>
2025-09-16 19:27:57 -0300

    Removed unnecessary data

--- py.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, jsonify
-# TODO from colorama import Fore as color, Style, init
-from ctypes import * 
-
-import os, datetime, time, subprocess, json
-import platform, socket
-import psutil, cpuinfo
-
-run = subprocess.Popen("./output/main.exe")
